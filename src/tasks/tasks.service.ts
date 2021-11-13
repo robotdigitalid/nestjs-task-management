313b6b738c69c7fa19cba1ae6c964962762429d5
Haqi <haqi@buatan.id>
2021-11-13 20:42:07 +0700

    Add Create task feature

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
